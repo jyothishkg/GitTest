cf9eb76798dae90d913e6296168ad2e07bf1170c
jyothish <jyothish@192.168.0.4>
2018-11-14 22:01:51 +0530

    new file uploading

---
+++ src/pack/GitClass2.java
@@ -0,0 +1,15 @@
+package pack;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GitClass2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver=new ChromeDriver();
+		driver.get("http://www.google.com");
+
+	}
+
+}
