ca3d58127c14987d57d9daa502b4412db4ee4d23
jyothish <jyothish@192.168.0.4>
2018-11-15 06:41:27 +0530

    added testng file and configured maven

---
+++ src/pack/MavenTestNG.java
@@ -0,0 +1,13 @@
+package pack;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class MavenTestNG {
+@Test
+public void test()
+{
+	Reporter.log("testing maven",true);
+}
+
+}
